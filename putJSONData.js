268fd227729030d0e5b669eb2ef9719eb55496cd
jmcg0192 <94491264+jmcg0192@users.noreply.github.com>
2025-05-01 20:21:52 -0400

    Add files via upload

--- putJSONData.js
+++ putJSONData.js
@@ -1,6 +1,6 @@
 const putJSONData = async (updatedData) => {
-	const binId = '67a2e9b0ad19ca34f8fa08de';
-	const apiKey = '$2a$10$ann475gv9wsfbmNHPxuOqefmVOcYihvVP2LJ36GP0KgsIdxo2uJJm';
+	const binId = '68124a478561e97a500ae25e';
+	const apiKey = '$2a$10$4gNjie2gLr.zVaIgMqTnuO79WGbCT4MSeM7jEejg8GiLpR.RnnQJ6';
 	const url = "https://api.jsonbin.io/v3/b/" + binId;
   
 	try {
@@ -18,7 +18,7 @@ const putJSONData = async (updatedData) => {
 		throw new Error('Failed to update data');
 	  }
   
-	  //alert('Data successfully updated'); //Removed this, as every time I deleted an album it threw a popup you had to click off of
+	  alert('Data successfully updated'); //Removed this, as every time I deleted an album it threw a popup you had to click off of
 	  return await response.json();
 	} catch (error) {
 	  console.error('Error:', error.message);
